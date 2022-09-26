0df6efcbcac083ac850c6cad4ed67c5724b9e168
Wenxuan Zhu <wxzhu@ggg.eecs.umich.edu>
2022-09-26 17:09:45 -0400

    customized run.sh

--- run.sh
+++ run.sh
@@ -1,32 +1,43 @@
-PATH2LIB=~/HW2/build/HW2/LLVMHW2.so        # Specify your build directory in the project
+set -Eeuxo pipefail
+
+PATH2LIB=~/hw2/build/HW2/LLVMHW2.so        # Specify your build directory in the project
 PASS=-fplicm-correctness                   # Choose either -fplicm-correctness or -fplicm-performance
 
 # Delete outputs from previous run.
-rm -f default.profraw ${1}_prof ${1}_fplicm ${1}_no_fplicm *.bc ${1}.profdata *_output *.ll
+git clean -fXd
+
+# rebuild HW2PASS
+mkdir build
+cd build
+cmake .. &> /dev/null
+make -j2
+cd ..
 
 # Convert source code to bitcode (IR)
-clang -emit-llvm -c ${1}.c -o ${1}.bc
+clang -emit-llvm -c ${1%.*}.c -o ${1%.*}.bc
 # Canonicalize natural loops
-opt -enable-new-pm=0 -loop-simplify ${1}.bc -o ${1}.ls.bc
+opt -enable-new-pm=0 -loop-simplify ${1%.*}.bc -o ${1%.*}.ls.bc
 # Instrument profiler
-opt -enable-new-pm=0 -pgo-instr-gen -instrprof ${1}.ls.bc -o ${1}.ls.prof.bc
+opt -enable-new-pm=0 -pgo-instr-gen -instrprof ${1%.*}.ls.bc -o ${1%.*}.ls.prof.bc
 # Generate binary executable with profiler embedded
-clang -fprofile-instr-generate ${1}.ls.prof.bc -o ${1}_prof
+clang -fprofile-instr-generate ${1%.*}.ls.prof.bc -o ${1%.*}_prof
 
 # Generate profiled data
-./${1}_prof > correct_output
-llvm-profdata merge -o ${1}.profdata default.profraw
+./${1%.*}_prof > correct_output
+llvm-profdata merge -o ${1%.*}.profdata default.profraw
 
 # Apply FPLICM
-opt -enable-new-pm=0 -o ${1}.fplicm.bc -pgo-instr-use -pgo-test-profile-file=${1}.profdata -load ${PATH2LIB} ${PASS} < ${1}.ls.bc > /dev/null
+opt -enable-new-pm=0 -o ${1%.*}.fplicm.bc -pgo-instr-use -pgo-test-profile-file=${1%.*}.profdata -load ${PATH2LIB} ${PASS} < ${1%.*}.ls.bc > /dev/null
 
 # Generate binary excutable before FPLICM: Unoptimzied code
-clang ${1}.ls.bc -o ${1}_no_fplicm
+clang ${1%.*}.ls.bc -o ${1%.*}_no_fplicm
 # Generate binary executable after FPLICM: Optimized code
-clang ${1}.fplicm.bc -o ${1}_fplicm
+clang ${1%.*}.fplicm.bc -o ${1%.*}_fplicm
 
 # Produce output from binary to check correctness
-./${1}_fplicm > fplicm_output
+./${1%.*}_fplicm > fplicm_output
+
+set +x
 
 echo -e "\n=== Correctness Check ==="
 if [ "$(diff correct_output fplicm_output)" != "" ]; then
@@ -35,12 +46,12 @@ else
     echo -e ">> PASS\n"
     # Measure performance
     echo -e "1. Performance of unoptimized code"
-    time ./${1}_no_fplicm > /dev/null
+    time ./${1%.*}_no_fplicm > /dev/null
     echo -e "\n\n"
     echo -e "2. Performance of optimized code"
-    time ./${1}_fplicm > /dev/null
+    time ./${1%.*}_fplicm > /dev/null
     echo -e "\n\n"
 fi
 
 # Cleanup
-rm -f default.profraw ${1}_prof ${1}_fplicm ${1}_no_fplicm *.bc ${1}.profdata *_output *.ll
+git clean -fXd > /dev/null
