62b0c2f13471ccb3767c7208744e8be00cba1185
Wenxuan <wxzhu@umich.edu>
2022-09-26 17:13:31 -0400

    customized run.sh more

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 set -Eeuxo pipefail
 
 PATH2LIB=~/hw2/build/HW2/LLVMHW2.so        # Specify your build directory in the project
-PASS=-fplicm-correctness                   # Choose either -fplicm-correctness or -fplicm-performance
+PASS=${1}                # Choose either -fplicm-correctness or -fplicm-performance
+TARGET=${2%.*}
 
 # Delete outputs from previous run.
 git clean -fXd
@@ -14,28 +15,28 @@ make -j2
 cd ..
 
 # Convert source code to bitcode (IR)
-clang -emit-llvm -c ${1%.*}.c -o ${1%.*}.bc
+clang -emit-llvm -c ${TARGET}.c -o ${TARGET}.bc
 # Canonicalize natural loops
-opt -enable-new-pm=0 -loop-simplify ${1%.*}.bc -o ${1%.*}.ls.bc
+opt -enable-new-pm=0 -loop-simplify ${TARGET}.bc -o ${TARGET}.ls.bc
 # Instrument profiler
-opt -enable-new-pm=0 -pgo-instr-gen -instrprof ${1%.*}.ls.bc -o ${1%.*}.ls.prof.bc
+opt -enable-new-pm=0 -pgo-instr-gen -instrprof ${TARGET}.ls.bc -o ${TARGET}.ls.prof.bc
 # Generate binary executable with profiler embedded
-clang -fprofile-instr-generate ${1%.*}.ls.prof.bc -o ${1%.*}_prof
+clang -fprofile-instr-generate ${TARGET}.ls.prof.bc -o ${TARGET}_prof
 
 # Generate profiled data
-./${1%.*}_prof > correct_output
-llvm-profdata merge -o ${1%.*}.profdata default.profraw
+./${TARGET}_prof > correct_output
+llvm-profdata merge -o ${TARGET}.profdata default.profraw
 
 # Apply FPLICM
-opt -enable-new-pm=0 -o ${1%.*}.fplicm.bc -pgo-instr-use -pgo-test-profile-file=${1%.*}.profdata -load ${PATH2LIB} ${PASS} < ${1%.*}.ls.bc > /dev/null
+opt -enable-new-pm=0 -o ${TARGET}.fplicm.bc -pgo-instr-use -pgo-test-profile-file=${TARGET}.profdata -load ${PATH2LIB} ${PASS} < ${TARGET}.ls.bc > /dev/null
 
 # Generate binary excutable before FPLICM: Unoptimzied code
-clang ${1%.*}.ls.bc -o ${1%.*}_no_fplicm
+clang ${TARGET}.ls.bc -o ${TARGET}_no_fplicm
 # Generate binary executable after FPLICM: Optimized code
-clang ${1%.*}.fplicm.bc -o ${1%.*}_fplicm
+clang ${TARGET}.fplicm.bc -o ${TARGET}_fplicm
 
 # Produce output from binary to check correctness
-./${1%.*}_fplicm > fplicm_output
+./${TARGET}_fplicm > fplicm_output
 
 set +x
 
@@ -46,10 +47,10 @@ else
     echo -e ">> PASS\n"
     # Measure performance
     echo -e "1. Performance of unoptimized code"
-    time ./${1%.*}_no_fplicm > /dev/null
+    time ./${TARGET}_no_fplicm > /dev/null
     echo -e "\n\n"
     echo -e "2. Performance of optimized code"
-    time ./${1%.*}_fplicm > /dev/null
+    time ./${TARGET}_fplicm > /dev/null
     echo -e "\n\n"
 fi
 
