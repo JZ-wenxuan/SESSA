99f63be22ce24c32f2a69f8d0d0fa400d89e86c7
Wenxuan <wxzhu@umich.edu>
2022-11-06 18:56:19 -0500

    clean sessa

--- HW2/HW2PASS.cpp
+++ HW2/HW2PASS.cpp
@@ -1,357 +1,99 @@
-//===-- Frequent Path Loop Invariant Code Motion Pass ------------------------===//
-//
-//                     The LLVM Compiler Infrastructure
-//
-// This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
-//
-//===----------------------------------------------------------------------===//
-//
-// EECS583 F22 - This pass can be used as a template for your Frequent Path LICM
-//               homework assignment. The pass gets registered as "fplicm".
-//
-// This pass performs loop invariant code motion, attempting to remove as much
-// code from the body of a loop as possible.  It does this by either hoisting
-// code into the preheader block, or by sinking code to the exit blocks if it is
-// safe. 
-//
-////===----------------------------------------------------------------------===//
-#include "llvm/Analysis/LoopInfo.h"
-#include "llvm/Analysis/LoopIterator.h"
-#include "llvm/Analysis/LoopPass.h"
-#include "llvm/IR/CFG.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/IR/LLVMContext.h"
-#include "llvm/Support/Debug.h"
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Transforms/Scalar/LoopPassManager.h"
-#include "llvm/Transforms/Scalar/LICM.h"
-#include "llvm/Transforms/Utils/BasicBlockUtils.h"
-#include "llvm/Transforms/Utils/LoopUtils.h"
-#include "llvm/Transforms/Utils/SSAUpdater.h"
-#include "llvm/Analysis/BranchProbabilityInfo.h"
-#include "llvm/Analysis/BlockFrequencyInfo.h"
-
-/* *******Implementation Starts Here******* */
-// include necessary header files
-/* *******Implementation Ends Here******* */
+ #include "llvm/ADT/ArrayRef.h"
+ #include "llvm/ADT/DenseMap.h"
+ #include "llvm/ADT/STLExtras.h"
+ #include "llvm/ADT/SmallPtrSet.h"
+ #include "llvm/ADT/SmallVector.h"
+ #include "llvm/ADT/Statistic.h"
+ #include "llvm/ADT/Twine.h"
+ #include "llvm/Analysis/AssumptionCache.h"
+ #include "llvm/Analysis/InstructionSimplify.h"
+ #include "llvm/Analysis/IteratedDominanceFrontier.h"
+ #include "llvm/Analysis/ValueTracking.h"
+ #include "llvm/IR/BasicBlock.h"
+ #include "llvm/IR/CFG.h"
+ #include "llvm/IR/Constant.h"
+ #include "llvm/IR/Constants.h"
+ #include "llvm/IR/DIBuilder.h"
+ #include "llvm/IR/DebugInfo.h"
+ #include "llvm/IR/Dominators.h"
+ #include "llvm/IR/Function.h"
+ #include "llvm/IR/InstrTypes.h"
+ #include "llvm/IR/Instruction.h"
+ #include "llvm/IR/Instructions.h"
+ #include "llvm/IR/IntrinsicInst.h"
+ #include "llvm/IR/Intrinsics.h"
+ #include "llvm/IR/LLVMContext.h"
+ #include "llvm/IR/Module.h"
+ #include "llvm/IR/Type.h"
+ #include "llvm/IR/User.h"
+ #include "llvm/Support/Casting.h"
+ #include "llvm/Transforms/Utils/Local.h"
+ #include "llvm/Transforms/Utils/PromoteMemToReg.h"
+ #include <algorithm>
+ #include <cassert>
+ #include <iterator>
+ #include <utility>
+ #include <vector>
 
 using namespace llvm;
 
-#define DEBUG_TYPE "fplicm"
+namespace {
 
-struct FPLICM {
-  Loop* L;
-  LoopInfo* LI;
-  BranchProbabilityInfo* BPI;
-  SmallPtrSet<BasicBlock *, 8> FrequentPath;
-  bool isPerformance;
-
-  FPLICM(Loop *L, LoopInfo *LI, BranchProbabilityInfo *BPI, bool isPerformance=false)
-    : L(L), LI(LI), BPI(BPI), isPerformance(isPerformance) {
-  }
-
-  bool run() {
-    LoopBlocksRPO Worklist(L);
-    Worklist.perform(LI);
-    // determine most likely path
-    auto BB = L->getHeader();
-    while (true) {
-      errs() << BB->getName();
-      FrequentPath.insert(BB);
-      auto NextBB = getMostLikelySuccessor(BB);
-      if (!NextBB) {
-        errs() << " has no most likely path.\n";
+bool isAllocaPromotable(const AllocaInst *AI) {
+  // Only allow direct and non-volatile loads and stores...
+  for (const User *U : AI->users()) {
+    if (const LoadInst *LI = dyn_cast<LoadInst>(U)) {
+      // Note that atomic loads can be transformed; atomic semantics do
+      // not have any meaning for a local alloca.
+      if (LI->isVolatile() || LI->getType() != AI->getAllocatedType())
         return false;
-      }
-      errs() << " -> " << NextBB->getName() << "\n";
-      if (NextBB == L->getHeader()) {
-        break;
-      }
-      BB = NextBB;
-    }
-  
-    // traverse FreqentPath (not in order) to find invariant pointers
-    // their loads should be hoisted, and their stores should be fixed-up
-    // std::vector<Value *> InvariantPtrs;
-    std::vector<Instruction *> AllHoisted;
-    std::vector<Instruction *> LoadHoisted;
-    for (BasicBlock *BB: FrequentPath) {
-      errs() << BB->getName() << ":\n";
-      for (Instruction &I : make_early_inc_range(*BB)) {
-        if (hasAlmostInvariantOperands(&I)) {
-          if (I.getOpcode() == Instruction::Load && isPtrAlmostInvariant(cast<LoadInst>(&I)->getPointerOperand())) {
-            errs() << "HOISTING >>>";
-            // hoist loads from almost invariant mem
-            I.moveBefore(L->getLoopPreheader()->getTerminator());
-            I.updateLocationAfterHoist();
-            LoadHoisted.push_back(&I);
-            AllHoisted.push_back(&I);
-          } else if (isPerformance && I.getOpcode() == Instruction::Store && isMSSA(cast<StoreInst>(&I))) {
-            errs() << "HOISTING >>>";
-            // hoist MSSA stores
-            I.moveBefore(L->getLoopPreheader()->getTerminator());
-            I.updateLocationAfterHoist();
-            AllHoisted.push_back(&I);
-          } else if (isPerformance && canHoist(I)) {
-            // hoist any non-memory operations
-            errs() << "HOISTING >>>";
-            I.moveBefore(L->getLoopPreheader()->getTerminator());
-            I.updateLocationAfterHoist();
-            AllHoisted.push_back(&I);
-
-          }
-        }
-        I.print(errs());
-        errs() << "\n";
-      }
-    }
-
-    std::vector<StoreInst *> SIs;
-    for (auto LI : LoadHoisted) {
-      SmallVector<PHINode *, 16> NewPHIs;
-      SSAUpdater SSA(&NewPHIs);
-      SSA.Initialize(LI->getType(), "phi");
-      // collect loaded value
-      SSA.AddAvailableValue(LI->getParent(), LI);
-      // for all stores breaking the hoisted load
-      for (Value *user: LI->getOperand(0)->users()) {
-        if (StoreInst *SI = dyn_cast<StoreInst>(user)) {
-          if (L->contains(SI)) {
-            // collect stored value
-            SSA.AddAvailableValue(SI->getParent(), SI->getValueOperand());
-            // prepare for fixup
-            SIs.push_back(cast<StoreInst>(SI));
-          }
-        }
-      }
-      // resolve phi for all direct uses of LI
-      std::vector<Use *> uses;
-      for (Use &use: LI->uses()) {
-        uses.push_back(&use);
-      }
-      for (auto i : uses) {
-        SSA.RewriteUseAfterInsertions(*i);
-      }
-    }
-
-    // insert subsequent fixup
-    std::vector<Instruction *> Fixups;
-    for (auto SI : SIs) {
-      ValueToValueMapTy vmap;
-      auto *InsertPos = SI->getNextNode();
-      for (auto HI : AllHoisted) {
-        auto *FI = HI->clone(); // fixup instruction
-        FI->insertBefore(InsertPos);
-        vmap[HI] = FI;
-        RemapInstruction(FI, vmap, RF_NoModuleLevelChanges | RF_IgnoreMissingLocals);
-        errs() << "Fixup >>>";
-        FI->print(errs());
-        errs() << "\n";
-      }
-      Fixups.push_back(SI->getNextNode());
-    }
-
-    // resolve fixup ssa
-    for (auto HI : AllHoisted) {
-      errs() << "SSA >>>";
-      HI->print(errs());
-      errs() << "\n";
-      // ignore stores
-      if (HI->getOpcode() == Instruction::Store) {
-        for (auto & FI : Fixups) FI = FI->getNextNode();
-        continue;
-      }
-      SmallVector<PHINode *, 16> NewPHIs;
-      SSAUpdater SSA(&NewPHIs);
-      SSA.Initialize(HI->getType(), "phi");
-      SSA.AddAvailableValue(HI->getParent(), HI);
-      for (auto & FI : Fixups) {
-        errs() << "    >>>";
-        FI->print(errs());
-        errs() << "\n";
-        SSA.AddAvailableValue(FI->getParent(), FI);
-        FI = FI->getNextNode();
-      }
-      // resolve phi for all uses of HI
-      std::vector<Use *> uses;
-      for (Use &use: HI->uses()) {
-        uses.push_back(&use);
-      }
-      for (auto i : uses) {
-        SSA.RewriteUseAfterInsertions(*i);
-      }
-    }
-
-    return true;
-  }
-
-  void fixupUses(Value *V, SSAUpdater &SSA) {
-    std::vector<Use *> uses;
-    for (Use &use: V->uses()) {
-      uses.push_back(&use);
-    }
-    for (auto i : uses) {
-      SSA.RewriteUseAfterInsertions(*i);
-    }
-  }
-
-  bool isPtrAlmostInvariant(const Value *Ptr) {
-    // return if no user of V is a store in the frequent path
-    return none_of(Ptr->users(), [this](const Value *V) {
-      if (const Instruction *I = dyn_cast<Instruction>(V))
-        return I->getOpcode() == Instruction::Store &&
-               FrequentPath.count(I->getParent()) != 0;
-      return false; // non-instruction is not store
-    });
-  }
-
-  bool isMSSA(const StoreInst *SI) {
-    // return if Ptr is stored only once
-    return none_of(SI->getPointerOperand()->users(), [&](const Value *V) {
-      // return if V is used in another store
-      if (const Instruction *I = dyn_cast<Instruction>(V))
-        return I->getOpcode() == Instruction::Store && I != SI;
-      return false; // non-instruction is not store
-    });
-  }
-
-  bool isAlmostInvariant(const Value *V) {
-    if (const Instruction *I = dyn_cast<Instruction>(V))
-      return FrequentPath.count(I->getParent()) == 0;
-    return true; // All non-instructions are loop invariant
-  }
-
-  bool hasAlmostInvariantOperands(const Instruction *I) {
-    return all_of(I->operands(), [this](const Value *V) {
-      return isAlmostInvariant(V);
-    });
-  }
-
-  BasicBlock *getMostLikelySuccessor(const BasicBlock *BB) {
-    auto I = BB->getTerminator();
-    for (unsigned i = 0; i < I->getNumSuccessors(); i++) {
-      const auto &Prob = BPI->getEdgeProbability(BB, i);
-      if ((double) Prob.getNumerator() >= 1717986918) {
-        errs() << " " << Prob.getNumerator() / ( Prob.getDenominator() / 100) << "%";
-        return I->getSuccessor(i);
-      }
-    }
-    return nullptr;
-  }
-
-  bool canHoist(Instruction &I) {
-    switch (I.getOpcode()) {
-    case Instruction::Add:
-    case Instruction::Sub:
-    case Instruction::Mul:
-    case Instruction::UDiv:
-    case Instruction::SDiv:
-    case Instruction::URem:
-    case Instruction::Shl:
-    case Instruction::LShr:
-    case Instruction::AShr:
-    case Instruction::And:
-    case Instruction::Or:
-    case Instruction::Xor:
-    case Instruction::ICmp:
-    case Instruction::SRem:
-    case Instruction::FAdd:
-    case Instruction::FSub:
-    case Instruction::FMul:
-    case Instruction::FDiv:
-    case Instruction::FRem:
-    case Instruction::FCmp:
-    case Instruction::GetElementPtr:
-    case Instruction::SExt:
-    case Instruction::Call:
-        return true;
-    default:
+    } else if (const StoreInst *SI = dyn_cast<StoreInst>(U)) {
+      if (SI->getValueOperand() == AI ||
+          SI->getValueOperand()->getType() != AI->getAllocatedType())
+        return false; // Don't allow a store OF the AI, only INTO the AI.
+      // Note that atomic stores can be transformed; atomic semantics do
+      // not have any meaning for a local alloca.
+      if (SI->isVolatile())
         return false;
+    } else if (const IntrinsicInst *II = dyn_cast<IntrinsicInst>(U)) {
+      if (!II->isLifetimeStartOrEnd() && !II->isDroppable())
+        return false;
+    } else if (const BitCastInst *BCI = dyn_cast<BitCastInst>(U)) {
+      if (!onlyUsedByLifetimeMarkersOrDroppableInsts(BCI))
+        return false;
+    } else if (const GetElementPtrInst *GEPI = dyn_cast<GetElementPtrInst>(U)) {
+      if (!GEPI->hasAllZeroIndices())
+        return false;
+      if (!onlyUsedByLifetimeMarkersOrDroppableInsts(GEPI))
+        return false;
+    } else if (const AddrSpaceCastInst *ASCI = dyn_cast<AddrSpaceCastInst>(U)) {
+      if (!onlyUsedByLifetimeMarkers(ASCI))
+        return false;
+    } else {
+      return false;
     }
   }
-};
 
-namespace Correctness{
-struct FPLICMPass : public LoopPass {
+  return true;
+}
+
+struct SESSA : public FunctionPass {
   static char ID;
-  FPLICMPass() : LoopPass(ID) {}
+  SESSA() : FunctionPass(ID) {}
 
-  bool runOnLoop(Loop *L, LPPassManager &LPM) override {
-    bool Changed = false;
-
-    /* *******Implementation Starts Here******* */
-    FPLICM Helper(
-      L,
-      &getAnalysis<LoopInfoWrapperPass>().getLoopInfo(),
-      &getAnalysis<BranchProbabilityInfoWrapperPass>().getBPI()
-    );
-    Changed = Helper.run();
-    /* *******Implementation Ends Here******* */
-    
-    return Changed;
+  bool runOnFunction(Function &F) override {
+    errs() << "SESSA: ";
+    errs().write_escaped(F.getName()) << '\n';
+    return false;
   }
+}; // end of struct SESSA
 
+}  // end of anonymous namespace
 
-  void getAnalysisUsage(AnalysisUsage &AU) const override {
-    AU.addRequired<BranchProbabilityInfoWrapperPass>();
-    AU.addRequired<BlockFrequencyInfoWrapperPass>();
-    AU.addRequired<LoopInfoWrapperPass>();
-  }
-
-private:
-  /// Little predicate that returns true if the specified basic block is in
-  /// a subloop of the current one, not the current one itself.
-  bool inSubLoop(BasicBlock *BB, Loop *CurLoop, LoopInfo *LI) {
-    assert(CurLoop->contains(BB) && "Only valid if BB is IN the loop");
-    return LI->getLoopFor(BB) != CurLoop;
-  }
-
-};
-} // end of namespace Correctness
-
-char Correctness::FPLICMPass::ID = 0;
-static RegisterPass<Correctness::FPLICMPass> X("fplicm-correctness", "Frequent Loop Invariant Code Motion for correctness test", false, false);
-
-
-namespace Performance{
-struct FPLICMPass : public LoopPass {
-  static char ID;
-  FPLICMPass() : LoopPass(ID) {}
-
-  bool runOnLoop(Loop *L, LPPassManager &LPM) override {
-    bool Changed = false;
-
-    /* *******Implementation Starts Here******* */
-    FPLICM Helper(
-      L,
-      &getAnalysis<LoopInfoWrapperPass>().getLoopInfo(),
-      &getAnalysis<BranchProbabilityInfoWrapperPass>().getBPI(),
-      true
-    );
-    Changed = Helper.run();
-    /* *******Implementation Ends Here******* */
-    
-    return Changed;
-  }
-
-
-  void getAnalysisUsage(AnalysisUsage &AU) const override {
-    AU.addRequired<BranchProbabilityInfoWrapperPass>();
-    AU.addRequired<BlockFrequencyInfoWrapperPass>();
-    AU.addRequired<LoopInfoWrapperPass>();
-  }
-
-private:
-  /// Little predicate that returns true if the specified basic block is in
-  /// a subloop of the current one, not the current one itself.
-  bool inSubLoop(BasicBlock *BB, Loop *CurLoop, LoopInfo *LI) {
-    assert(CurLoop->contains(BB) && "Only valid if BB is IN the loop");
-    return LI->getLoopFor(BB) != CurLoop;
-  }
-
-}; 
-} // end of namespace Performance
-
-char Performance::FPLICMPass::ID = 0;
-static RegisterPass<Performance::FPLICMPass> Y("fplicm-performance", "Frequent Loop Invariant Code Motion for performance test", false, false);
+char SESSA::ID = 0;
+static RegisterPass<SESSA> X("sessa", "SESSA World Pass",
+                             false /* Only looks at CFG */,
+                             false /* Analysis Pass */);
--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 set -Eeuxo pipefail
 
-PASSLIB=~/hw2/build/HW2/LLVMHW2.so        # Specify your build directory in the project
+PASSLIB=~/proj/build/HW2/LLVMHW2.so        # Specify your build directory in the project
 TARGET=${1%.*}
-PASS=${@:2}                # Choose either -fplicm-correctness or -fplicm-performance
+PASS=${@:2}                # Choose either -sessa-correctness or -sessa-performance
 
 # Delete outputs from previous run.
 git clean -fXd > /dev/null
@@ -17,44 +17,50 @@ cd ..
 # Convert source code to bitcode (IR)
 clang -emit-llvm -c -Xclang -disable-O0-optnone ${TARGET}.c -o ${TARGET}.bc
 # Canonicalize natural loops
-opt -enable-new-pm=0 -loop-simplify ${TARGET}.bc -o ${TARGET}.ls.bc
-# Instrument profiler
-opt -enable-new-pm=0 -pgo-instr-gen -instrprof ${TARGET}.ls.bc -o ${TARGET}.ls.prof.bc
-# Generate binary executable with profiler embedded
-clang -fprofile-instr-generate ${TARGET}.ls.prof.bc -o ${TARGET}_prof
+# opt -enable-new-pm=0 -loop-simplify ${TARGET}.bc -o ${TARGET}.bc
+# # Instrument profiler
+# opt -enable-new-pm=0 -pgo-instr-gen -instrprof ${TARGET}.bc -o ${TARGET}.prof.bc
+# # Generate binary executable with profiler embedded
+# clang -fprofile-instr-generate ${TARGET}.prof.bc -o ${TARGET}_prof
 
-# Generate profiled data
-./${TARGET}_prof > correct_output
-llvm-profdata merge -o ${TARGET}.profdata default.profraw
+# # Generate profiled data
+# ./${TARGET}_prof > correct_output
+# llvm-profdata merge -o ${TARGET}.profdata default.profraw
 
-# Apply FPLICM
-opt -enable-new-pm=0 -o ${TARGET}.fplicm.bc -pgo-instr-use -pgo-test-profile-file=${TARGET}.profdata -load ${PASSLIB} ${PASS} < ${TARGET}.ls.bc &> opt_output
+# Apply sessa
+opt -enable-new-pm=0 -o ${TARGET}.sessa.bc -load ${PASSLIB} ${PASS} < ${TARGET}.bc &> opt_output
+opt -enable-new-pm=0 -o ${TARGET}.cytron.bc -mem2reg < ${TARGET}.bc &> /dev/null
 
-# Generate binary excutable before FPLICM: Unoptimzied code
-llvm-dis ${TARGET}.ls.bc -o=no_fplicm.ll
-clang -S ${TARGET}.ls.bc -o no_fplicm.s
-clang ${TARGET}.ls.bc -o ${TARGET}_no_fplicm
-# Generate binary executable after FPLICM: Optimized code
-llvm-dis ${TARGET}.fplicm.bc -o=fplicm.ll
-clang -S ${TARGET}.fplicm.bc -o fplicm.s
-clang ${TARGET}.fplicm.bc -o ${TARGET}_fplicm
+# Generate binary excutable before ssa construction: Unoptimzied code
+llvm-dis ${TARGET}.bc -o=original.ll
+clang -S ${TARGET}.bc -o original.s
+clang ${TARGET}.bc -o ${TARGET}_original
+# Generate binary excutable after cytron
+llvm-dis ${TARGET}.cytron.bc -o=cytron.ll
+clang -S ${TARGET}.cytron.bc -o cytron.s
+clang ${TARGET}.cytron.bc -o ${TARGET}_cytron
+# Generate binary executable after sessa
+llvm-dis ${TARGET}.sessa.bc -o=sessa.ll
+clang -S ${TARGET}.sessa.bc -o sessa.s
+clang ${TARGET}.sessa.bc -o ${TARGET}_sessa
 
 # Produce output from binary to check correctness
-./${TARGET}_fplicm > fplicm_output
+./${TARGET}_sessa > sessa_output
+./${TARGET}_original > correct_output
 
 set +x
 
 echo -e "\n=== Correctness Check ==="
-if [ "$(diff correct_output fplicm_output)" != "" ]; then
+if [ "$(diff correct_output sessa_output)" != "" ]; then
     echo -e ">> FAIL\n"
     exit 1
 else
     echo -e ">> PASS\n"
-    # Measure performance
-    echo -e "1. Performance of unoptimized code"
-    time ./${TARGET}_no_fplicm > /dev/null
-    echo -e "\n\n"
-    echo -e "2. Performance of optimized code"
-    time ./${TARGET}_fplicm > /dev/null
-    echo -e "\n\n"
+    # # Measure performance
+    # echo -e "1. Performance of unoptimized code"
+    # time ./${TARGET}_cytron > /dev/null
+    # echo -e "\n\n"
+    # echo -e "2. Performance of optimized code"
+    # time ./${TARGET}_sessa > /dev/null
+    # echo -e "\n\n"
 fi
